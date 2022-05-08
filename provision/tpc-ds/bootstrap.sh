fe3378cdf6d41d219981f55aec80f622924d00a9
Eduards Gruberts <eduards.gruberts@gogetssl.com>
2022-05-08 10:17:56 +0300

    add coments

--- provision/tpc-ds/bootstrap.sh
+++ provision/tpc-ds/bootstrap.sh
@@ -1,8 +1,15 @@
+query_optimization_benchmark_stand/provision/tpc-ds
+
 createdb tpcds
-psql tpcds -f tpcds.sql
+psql tpcds -f ./query/tpcds.sql
 
 mkdir -p /tmp/tpcds/
 
+
+#sudo apt-get install gcc make flex bison byacc git
+#make clean
+#make OS=LINUX
+#Skompilirovannaja versija uze v repo
 ./dsdgen -scale 1 -FORCE -VERBOSE -dir /tmp/tpcds/
 
 ./dsqgen -DIRECTORY ../query_templates -INPUT ../query_templates/templates.lst -VERBOSE Y -QUALIFY Y -DIALECT netezza
