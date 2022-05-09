d66233876de1ac7f3ee0777694733c23fa40aa77
Eduards Gruberts <eduards.gruberts@gogetssl.com>
2022-05-09 07:14:03 +0300

    update sql and structure

--- provision/LDBC-BI/postgres/queries/interactive-complex-2.sql
+++ provision/LDBC-BI/postgres/queries/interactive-complex-2.sql
@@ -6,8 +6,8 @@ select p_personid, p_firstname, p_lastname, m_messageid, COALESCE(m_ps_imagefile
 from person, message, knows
 where
     p_personid = m_creatorid and
-    m_creationdate <= '2010-10-16' and
-    k_person1id = 10995116278009 and
+    m_creationdate <= :maxDate and
+    k_person1id = :personId and
     k_person2id = p_personid
 order by m_creationdate desc, m_messageid asc
 limit 20
